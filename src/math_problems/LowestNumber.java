0450c26e8ce35918fab4bc6d11ee47c2f0c6e886
Mustaphahammad <hmdloyalty@gmail.com>
2022-10-11 02:11:40 -0600

    LowestNumber.math_problems solved

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -1,5 +1,7 @@
 package math_problems;
 
+import java.util.Arrays;
+
 public class LowestNumber {
 
     /** INSTRUCTIONS
@@ -8,10 +10,13 @@ public class LowestNumber {
 
     public static void main(String[] args) {
 
-        int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
+        int[] array = new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
-
-
+        lowNumber(array);
     }
+         static void lowNumber ( int[] array){
+            Arrays.sort(array);
+            System.out.println("The lowest number of this array is : " + array[0]);
+        }
 
 }
