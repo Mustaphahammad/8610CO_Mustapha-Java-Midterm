8207f74bdb6cc012430f50993d1a7be7b4642a73
Mustaphahammad <hmdloyalty@gmail.com>
2022-10-10 22:29:45 -0600

    Fibonacci.math_problems solved

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -9,7 +9,21 @@ public class Fibonacci {
      **/
 
     public static void main(String[] args) {
-     }
+    int n1 = 0;
+    int n2 = 1;
+    int n = 40;
+    int counter = 0;
+    while(counter <= n) {
+        System.out.print(n1 +",");
+        int n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        counter++;
+    }
+
+
+
+    }
 
     }
 
