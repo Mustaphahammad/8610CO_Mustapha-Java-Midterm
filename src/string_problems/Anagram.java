6f27bc35e54c232518cd41c5a7838b69a6fa3ed2
Mustaphahammad <hmdloyalty@gmail.com>
2022-10-13 02:56:43 -0600

    anagram.string_problems solved

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,5 +1,7 @@
 package string_problems;
 
+import java.util.Arrays;
+
 public class Anagram {
 
     /** INSTRUCTIONS
@@ -15,4 +17,36 @@ public class Anagram {
 
     // Implement Here
 
-}
+        public static void main(String[] args) {
+            String str1 = "cat";
+            String str2 = "act";
+
+            str1 = str1.toLowerCase();
+            str2 = str2.toLowerCase();
+
+            // First we need to exclude any empty String .
+            if (str1.isEmpty() || str2.isEmpty()) {
+                System.out.println("invalid input , String is empty!");
+            }
+            // check if both strings have the same length
+            else if(str1.length() == str2.length()) {
+
+                char[] charArray1 = str1.toCharArray();
+                char[] charArray2 = str2.toCharArray();
+
+                Arrays.sort(charArray1);
+                Arrays.sort(charArray2);
+
+                boolean result = Arrays.equals(charArray1, charArray2);
+
+                if(result) {
+                    System.out.println(str1 + " and " + str2 + " are anagram.");
+                } else {
+                    System.out.println(str1 + " and " + str2 + " are not anagram.");
+                }
+            } else {
+                System.out.println(str1 + " and " + str2 + " are not anagram.");
+            }
+        }
+    }
+
