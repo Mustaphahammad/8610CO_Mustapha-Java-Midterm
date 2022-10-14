46743ac59e01476e72468d81493c7e5625c4b52f
Mustaphahammad <hmdloyalty@gmail.com>
2022-10-14 01:55:32 -0600

    permutation.string_problems solved

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -2,14 +2,31 @@ package string_problems;
 
 public class Permutation {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Create a Java program to compute all permutations of any given string
-     *
+     * <p>
      * e.g. -  "ABC" = "ABC" "ACB" "BAC" "BCA" "CAB" "CBA"
      */
 
     public static void main(String[] args) {
-
+        permutation("lam");
     }
+
     // Implement Here
+    public static void permutation(String str) {
+        permutation("", str);
+    }
+
+    private static void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) {
+            System.out.print(prefix + " ");
+        } else {
+            for (int i = 0; i < n; i++)
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
+        }
+    }
+
+
 }
