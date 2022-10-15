d56f8f722a968fe086e01f6bddab1c1c875c6465
Mustaphahammad <hmdloyalty@gmail.com>
2022-10-15 00:50:15 -0600

    data_structures.UseMap solved

--- src/data_structures/UseMap.java
+++ src/data_structures/UseMap.java
@@ -1,17 +1,19 @@
 package data_structures;
 
+import com.fasterxml.jackson.databind.MappingIterator;
 import databases.SharedStepsDatabase;
 
 import java.util.*;
 
 public class UseMap {
 
-    /** INSTRUCTIONS
-     *
+    /**
+     * INSTRUCTIONS
+     * <p>
      * Implement code to demonstrate how to interact with a database connection, using a Map
      * You must insert the map into a table, and then you must submit & execute a query to retrieve
      * all the submitted data (retrieval does not need to be stored as a map - you can use any data structure)
-     *
+     * <p>
      * Use For-Each loop and While-loop with Iterator to retrieve data.
      */
 
@@ -36,5 +38,25 @@ public class UseMap {
 
         // IMPLEMENT HERE
 
-    }
+        //Using For-Each loop  to retrieve data.
+        System.out.println("-------------For-Each loop--------------");
+        map.forEach((k,v) -> System.out.println("key: "+k+" value:"+v));
+
+        //Using While-loop with Iterator to retrieve data.
+        System.out.println("--------------While-loop with Iterator---------------");
+
+        // Inserting data into DB
+          sql.insertMap(tableName, map);
+
+        // Execute the query to retrieve data
+        List dB_data = sql.executeQueryReadAll("select * from " + tableName);
+
+        Iterator it = dB_data.iterator();
+
+        //Using While-loop with Iterator to retrieve data
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+      sql.closeResources();
+}
 }
